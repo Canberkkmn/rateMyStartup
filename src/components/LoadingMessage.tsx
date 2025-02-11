ad48e0955d81bc675a1565df1f9cfa4e654e81b5
Canberkkmn <canberkkaman@gmail.com>
2025-02-12 00:04:58 +0300

    update loadingMessage

--- src/components/LoadingMessage.tsx
+++ src/components/LoadingMessage.tsx
@@ -1,5 +1,8 @@
 export default function LoadingMessage() {
   return (
-    <div className="text-center text-gray-600 text-xl mt-10">Loading...</div>
+    <div className="flex justify-center items-center h-20">
+      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+      <span className="ml-3 text-gray-900 font-medium">Loading...</span>
+    </div>
   );
 }
